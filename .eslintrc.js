e338b821aca1967c1a88eb83704e7972760fadb5
Joaobru <joaopaulobruno123@gmail.com>
2025-01-30 22:24:30 -0300

    feat: initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+};
